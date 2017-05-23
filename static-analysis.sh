085d642e30c6f5a90ed92ae66eaf7d8e01fdcc9d
Ziesche Til Newman <tilnewman@gmail.com>
2017-05-23 11:02:01 -0700

    Added a script that runs a set of static analysis tools.

---
+++ static-analysis.sh
@@ -0,0 +1,3 @@
+cppcheck -j 7  --enable=all .
+flint++ -r .
+cpplint --linelength=999 --recursive --filter=-whitespace,-readability/namespace,-readability/todo,-runtime/indentation_namespace,-build/include_order,-build/header_guard,-runtime/references,-legal/copyright,-readability/braces,-build/namespaces --quiet *
