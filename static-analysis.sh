057053ba4a2d20dfa4b24d2895950bd60942c357
Ziesche Til Newman <tilnewman@gmail.com>
2017-05-23 17:27:42 -0700

    Update static analysis settings.

--- static-analysis.sh
+++ static-analysis.sh
@@ -1,3 +1,3 @@
 cppcheck --enable=all .
 flint++ -r .
-cpplint --linelength=999 --recursive --filter=-whitespace,-readability/namespace,-readability/todo,-runtime/indentation_namespace,-build/include_order,-build/header_guard,-runtime/references,-legal/copyright,-readability/braces,-build/namespaces --quiet *
+cpplint --linelength=999 --recursive --filter=-whitespace,-readability/namespace,-readability/todo,-runtime/indentation_namespace,-build/include_order,-build/header_guard,-runtime/references,-legal/copyright,-readability/braces,-build/namespaces,-runtime/int,-readability/multiline_string,-build/c++11 --quiet *
