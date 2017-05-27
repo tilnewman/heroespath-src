a889e86cd644abe3288d77a30db87e95c9e68748
Ziesche Til Newman <tilnewman@gmail.com>
2017-05-27 08:51:15 -0700

    Updated static analysis suppressions to exclude unused functions.

--- run-static-analysis.sh
+++ run-static-analysis.sh
@@ -1,3 +1,3 @@
-cppcheck --enable=all src/
+cppcheck --enable=all --suppress=unusedFunction src/
 flint++ -r src/
 cpplint --linelength=999 --recursive --filter=-whitespace,-readability/namespace,-readability/todo,-runtime/indentation_namespace,-build/include_order,-build/header_guard,-runtime/references,-legal/copyright,-readability/braces,-build/namespaces,-runtime/int,-readability/multiline_string,-build/c++11 --quiet src/*
