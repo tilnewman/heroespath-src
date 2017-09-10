ac2da1aadd788d04b2c5e02ed914eeac730d440c
Ziesche Til Newman <tilnewman@gmail.com>
2017-09-09 18:32:55 -0700

    Made a unit testing harness using boost test and CTest

---
+++ src/HeroesPathTest/combat-tests.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_MODULE "HeroesPathTestModule_Main"
+
+#include <boost/test/unit_test.hpp>
+
+namespace utf = boost::unit_test;
+
+
+
+BOOST_AUTO_TEST_CASE( combat_test_1 )
+{
+    BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_CASE( combat_test_2 )
+{
+    BOOST_CHECK(true);
+}
+
---
+++ src/HeroesPathTest/main.cpp
@@ -0,0 +1,16 @@
+#define BOOST_TEST_MODULE "HeroesPathTestModule_Main"
+
+#include <boost/test/unit_test.hpp>
+
+namespace utf = boost::unit_test;
+
+
+BOOST_AUTO_TEST_CASE( main_test_1 )
+{
+    BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_CASE( main_test_2 )
+{
+    BOOST_CHECK(true);
+}
