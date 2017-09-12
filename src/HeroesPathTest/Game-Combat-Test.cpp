3b120487dec4a69351c4fd8718cb34fb3e33d0ec
Ziesche Til Newman <tilnewman@gmail.com>
2017-09-12 14:49:17 -0700

    Finished CombatTree tests.

--- src/HeroesPathTest/Game-Combat-Test.cpp
+++ src/HeroesPathTest/Game-Combat-Test.cpp
@@ -33,8 +33,11 @@
 #include "game/startup-shutdown.hpp"
 #include "game/state/game-state-factory.hpp"
 #include "game/player/fake-party.hpp"
+#include "game/player/party.hpp"
+#include "game/player/character.hpp"
 #include "game/combat/encounter.hpp"
 #include "game/combat/combat-tree.hpp"
+#include "game/combat/combat-node.hpp"
 #include "game/creature/creature.hpp"
 
 #include <exception>
@@ -180,11 +183,316 @@ BOOST_AUTO_TEST_CASE(CombatTree_DefaultParty)
     BOOST_CHECK_MESSAGE(game::StartupShutdown::Setup("Heroes' Path Unit Tests", 0, nullptr),
         "StartupShutdown::Setup() failed");
 
-    state::GameStateFactory::Instance()->NewGame(player::FakeParty::Make());
-
+    //setup the default party
+    auto partyPtr{ player::FakeParty::Make() };
+    state::GameStateFactory::Instance()->NewGame(partyPtr);
     combat::Encounter::Instance()->BeginCombatTasks();
 
-    //TODO
+    //make a CombatNode for the creature at position zero
+    auto const CREATURE_PTR0{
+        static_cast<creature::CreaturePtr_t>(partyPtr->Characters().at(0)) };
+
+    auto combatNodeSPtr0{ std::make_shared<combat::CombatNode>(CREATURE_PTR0) };
+    const int BLOCKING_POS0{ -13 };
+    combatNodeSPtr0->SetBlockingPos(BLOCKING_POS0);
+
+    //add that CombatNodeSPtr_t as a Vertex to the CombatTree
+    combat::CombatTree combatTree;
+    auto const VERT_ID0{ combatTree.AddVertex(combatNodeSPtr0) };
+
+    //tests after the single vertex insert
+    BOOST_CHECK(combatTree.NextAvailableId() == 1);
+    BOOST_CHECK(combatTree.GetNode(VERT_ID0) == combatNodeSPtr0.get());
+    BOOST_CHECK(combatTree.GetNodeSPtr(VERT_ID0) == combatNodeSPtr0);
+    BOOST_CHECK(combatTree.GetNode(CREATURE_PTR0) == combatNodeSPtr0.get());
+    BOOST_CHECK(combatTree.GetNodeSPtr(CREATURE_PTR0) == combatNodeSPtr0);
+    BOOST_CHECK(combatTree.GetNodeId(combatNodeSPtr0.get()) == VERT_ID0);
+    BOOST_CHECK(combatTree.GetNodeId(CREATURE_PTR0) == VERT_ID0);
+    BOOST_CHECK(combatTree.VertexCount() == 1);
+    BOOST_CHECK(combatTree.VertexCountByBlockingPos(0) == 0);
+    BOOST_CHECK(combatTree.VertexCountByBlockingPos(BLOCKING_POS0) == 1);
+    BOOST_CHECK(combatTree.Vertexes().size() == 1);
+    BOOST_CHECK(combatTree.VertexesString(false).empty() == false);
+    BOOST_CHECK(combatTree.EdgeCount() == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::All) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::Blocking) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::ShoulderToShoulder) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::Count) == 0);
+    BOOST_CHECK(combatTree.Edges().empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::All).empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::Blocking).empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::ShoulderToShoulder).empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::Count).empty());
+    BOOST_CHECK(combatTree.EdgesString(false).empty());
+    BOOST_CHECK(combatTree.DoesVertexExist(VERT_ID0) == true);
+    BOOST_CHECK(combatTree.DoesVertexExist(VERT_ID0 + 1) == false);
+    BOOST_CHECK(combatTree.CountAdjacent(VERT_ID0) == 0);
+    BOOST_CHECK(combatTree.AreAnyAdjacent(VERT_ID0) == false);
+    BOOST_CHECK(combatTree.GetBlockingPosMin() == BLOCKING_POS0);
+    BOOST_CHECK(combatTree.GetBlockingPosMax() == BLOCKING_POS0);
+    const combat::CombatTree::IdVec_t ID_VEC_SINGLE_ZERO = { 0 };
+    {
+        combat::CombatTree::IdVec_t ids;
+        combatTree.GetNodeIds(ids, CREATURE_PTR0->Role());
+        BOOST_CHECK(ids == ID_VEC_SINGLE_ZERO);
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        combatTree.GetNodeIds(ids, CREATURE_PTR0->Race());
+        BOOST_CHECK(ids == ID_VEC_SINGLE_ZERO);
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        combatTree.FindAdjacentByEdgeType(VERT_ID0, ids);
+        BOOST_CHECK(ids.empty());
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        combatTree.FindAdjacent(VERT_ID0, ids);
+        BOOST_CHECK(ids.empty());
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        BOOST_CHECK(combatTree.GetNodeIDsAtBlockingPos(ids, BLOCKING_POS0));
+        BOOST_CHECK(ids == ID_VEC_SINGLE_ZERO);
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        BOOST_CHECK(combatTree.GetNodeIDsAllAroundBlockingPos(ids, BLOCKING_POS0));
+        BOOST_CHECK(ids == ID_VEC_SINGLE_ZERO);
+    }
+    {
+        combat::CombatNodePVec_t combatNodePtrs;
+        BOOST_CHECK(combatTree.GetNodesAtBlockingPos(combatNodePtrs, BLOCKING_POS0));
+        BOOST_CHECK(combatNodePtrs.size() == 1);
+        BOOST_CHECK(combatNodePtrs.at(0) == combatNodeSPtr0.get());
+    }
+    {
+        combat::CombatNodePVec_t combatNodePtrs;
+        BOOST_CHECK(combatTree.GetNodesAllAroundBlockingPos(combatNodePtrs, BLOCKING_POS0));
+        BOOST_CHECK(combatNodePtrs.size() == 1);
+        BOOST_CHECK(combatNodePtrs.at(0) == combatNodeSPtr0.get());
+    }
+    {
+        combat::CombatNodePVec_t combatNodePtrs;
+        combatTree.GetCombatNodes(combatNodePtrs);
+        BOOST_CHECK(combatNodePtrs.size() == 1);
+        BOOST_CHECK(combatNodePtrs.at(0) == combatNodeSPtr0.get());
+    }
+
+    //make a CombatNode for the creature at position zero
+    auto const CREATURE_PTR1{
+        static_cast<creature::CreaturePtr_t>(partyPtr->Characters().at(1)) };
+
+    auto combatNodeSPtr1{ std::make_shared<combat::CombatNode>(CREATURE_PTR1) };
+    const int BLOCKING_POS1{ BLOCKING_POS0 };
+    combatNodeSPtr1->SetBlockingPos(BLOCKING_POS1);
+
+    //add that CombatNodeSPtr_t as a Vertex to the CombatTree
+    auto const VERT_ID1{ combatTree.AddVertex(combatNodeSPtr1) };
+
+    //tests after the second vertex insert
+    BOOST_CHECK(combatTree.NextAvailableId() == 2);
+    BOOST_CHECK(combatTree.GetNode(VERT_ID1) == combatNodeSPtr1.get());
+    BOOST_CHECK(combatTree.GetNodeSPtr(VERT_ID1) == combatNodeSPtr1);
+    BOOST_CHECK(combatTree.GetNode(CREATURE_PTR1) == combatNodeSPtr1.get());
+    BOOST_CHECK(combatTree.GetNodeSPtr(CREATURE_PTR1) == combatNodeSPtr1);
+    BOOST_CHECK(combatTree.GetNodeId(combatNodeSPtr1.get()) == VERT_ID1);
+    BOOST_CHECK(combatTree.GetNodeId(CREATURE_PTR1) == VERT_ID1);
+    BOOST_CHECK(combatTree.VertexCount() == 2);
+    BOOST_CHECK(combatTree.VertexCountByBlockingPos(0) == 0);
+    BOOST_CHECK(combatTree.VertexCountByBlockingPos(BLOCKING_POS1) == 2);
+    BOOST_CHECK(combatTree.Vertexes().size() == 2);
+    BOOST_CHECK(combatTree.VertexesString(false).empty() == false);
+    BOOST_CHECK(combatTree.EdgeCount() == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::All) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::Blocking) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::ShoulderToShoulder) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::Count) == 0);
+    BOOST_CHECK(combatTree.Edges().empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::All).empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::Blocking).empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::ShoulderToShoulder).empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::Count).empty());
+    BOOST_CHECK(combatTree.EdgesString(false).empty());
+    BOOST_CHECK(combatTree.DoesVertexExist(VERT_ID1) == true);
+    BOOST_CHECK(combatTree.DoesVertexExist(VERT_ID1 + 1) == false);
+    BOOST_CHECK(combatTree.CountAdjacent(VERT_ID1) == 0);
+    BOOST_CHECK(combatTree.AreAnyAdjacent(VERT_ID1) == false);
+    BOOST_CHECK(combatTree.GetBlockingPosMin() == BLOCKING_POS1);
+    BOOST_CHECK(combatTree.GetBlockingPosMax() == BLOCKING_POS1);
+    {
+        combat::CombatTree::IdVec_t ids;
+        BOOST_CHECK(combatTree.GetNodeIDsAtBlockingPos(ids, BLOCKING_POS1));
+        BOOST_CHECK(ids.size() == 2);
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        BOOST_CHECK(combatTree.GetNodeIDsAllAroundBlockingPos(ids, BLOCKING_POS1));
+        BOOST_CHECK(ids.size() == 2);
+    }
+    {
+        combat::CombatNodePVec_t combatNodePtrs;
+        BOOST_CHECK(combatTree.GetNodesAtBlockingPos(combatNodePtrs, BLOCKING_POS1));
+        BOOST_CHECK(combatNodePtrs.size() == 2);
+    }
+    {
+        combat::CombatNodePVec_t combatNodePtrs;
+        BOOST_CHECK(combatTree.GetNodesAllAroundBlockingPos(combatNodePtrs, BLOCKING_POS1));
+        BOOST_CHECK(combatNodePtrs.size() == 2);
+    }
+    {
+        combat::CombatNodePVec_t combatNodePtrs;
+        combatTree.GetCombatNodes(combatNodePtrs);
+        BOOST_CHECK(combatNodePtrs.size() == 2);
+    }
+
+    //add an edge to link the two creatures shoulder-to-shoulder
+    combatTree.AddEdge(VERT_ID0, VERT_ID1, combat::EdgeType::ShoulderToShoulder);
+
+    BOOST_CHECK(combatTree.DoesEdgeExist(VERT_ID0, VERT_ID1) == true);
+    BOOST_CHECK(combatTree.EdgeCount() == 1);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::All) == 1);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::Blocking) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::ShoulderToShoulder) == 1);
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::Blocking).empty());
+
+    {
+        auto const EDGES_ALL{ combatTree.Edges(combat::EdgeType::All) };
+        BOOST_CHECK(EDGES_ALL.size() == 1);
+
+        BOOST_CHECK(
+            ((std::get<0>(EDGES_ALL[0]) == 0) || (std::get<1>(EDGES_ALL[0]) == 1)) ||
+             (std::get<0>(EDGES_ALL[0]) == 1) || (std::get<1>(EDGES_ALL[0]) == 0));
+
+        BOOST_CHECK(std::get<2>(EDGES_ALL[0]) == combat::EdgeType::ShoulderToShoulder);
+    }
+
+    {
+        auto const EDGES_STS{ combatTree.Edges(combat::EdgeType::ShoulderToShoulder) };
+        BOOST_CHECK(EDGES_STS.size() == 1);
+
+        BOOST_CHECK(
+            ((std::get<0>(EDGES_STS[0]) == 0) || (std::get<1>(EDGES_STS[0]) == 1)) ||
+             (std::get<0>(EDGES_STS[0]) == 1) || (std::get<1>(EDGES_STS[0]) == 0));
+
+        BOOST_CHECK(std::get<2>(EDGES_STS[0]) == combat::EdgeType::ShoulderToShoulder);
+    }
+
+    combatTree.RemoveVertex(VERT_ID1, true);
+    BOOST_CHECK(combatTree.DoesEdgeExist(VERT_ID0, VERT_ID1) == false);
+    BOOST_CHECK(combatTree.DoesVertexExist(VERT_ID1) == false);
+    BOOST_CHECK(combatTree.DoesVertexExist(VERT_ID0) == true);
+    BOOST_CHECK(combatTree.EdgeCount() == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::All) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::ShoulderToShoulder) == 0);
+
+    BOOST_CHECK_THROW(combatTree.RemoveVertex(VERT_ID1), std::invalid_argument);
+
+    combatTree.RemoveVertex(VERT_ID0);
+
+    BOOST_CHECK(combatTree.DoesVertexExist(VERT_ID0) == false);
+    BOOST_CHECK_THROW(combatTree.RemoveVertex(VERT_ID0), std::invalid_argument);
+
+    //whole set of checks to be sure the combatTree is back in its inital (empty) state
+    BOOST_CHECK(combatTree.NextAvailableId() == 0);
+
+    BOOST_CHECK_THROW(combatTree.GetNode(
+        static_cast<combat::CombatTree::Id_t>(0)), std::invalid_argument);
+
+    BOOST_CHECK_THROW(combatTree.GetNodeSPtr(
+        static_cast<combat::CombatTree::Id_t>(0)), std::invalid_argument);
+
+    BOOST_CHECK(combatTree.GetNode(nullptr) == nullptr);
+    BOOST_CHECK(combatTree.GetNodeSPtr(nullptr) == nullptr);
+
+    BOOST_CHECK_THROW(combatTree.SetNode(0, combat::CombatNodeSPtr_t()), std::invalid_argument);
+
+    BOOST_CHECK_THROW(combatTree.GetNodeId(
+        combat::CombatNodePtr_t(nullptr)), std::invalid_argument);
+
+    BOOST_CHECK_THROW(combatTree.GetNodeId(
+        creature::CreaturePtr_t(nullptr)), std::invalid_argument);
+
+    BOOST_CHECK(combatTree.GetNode(0.0f, 0.0f) == nullptr);
+    BOOST_CHECK(combatTree.VertexCount() == 0);
+    BOOST_CHECK(combatTree.VertexCountByBlockingPos(0) == 0);
+    BOOST_CHECK(combatTree.Vertexes().empty());
+    BOOST_CHECK(combatTree.VertexesString(false).empty());
+    BOOST_CHECK_THROW(combatTree.RemoveVertex(0, false), std::invalid_argument);
+    BOOST_CHECK_THROW(combatTree.RemoveVertex(0, true), std::invalid_argument);
+    BOOST_CHECK(combatTree.EdgeCount() == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::All) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::Blocking) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::ShoulderToShoulder) == 0);
+    BOOST_CHECK(combatTree.EdgeCount(combat::EdgeType::Count) == 0);
+    BOOST_CHECK(combatTree.Edges().empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::All).empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::Blocking).empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::ShoulderToShoulder).empty());
+    BOOST_CHECK(combatTree.Edges(combat::EdgeType::Count).empty());
+    BOOST_CHECK(combatTree.EdgesString(false).empty());
+    BOOST_CHECK_THROW(combatTree.AddEdge(0, 0, combat::EdgeType::Blocking), std::invalid_argument);
+    BOOST_CHECK_THROW(combatTree.AddEdge(0, 1, combat::EdgeType::Blocking), std::invalid_argument);
+    BOOST_CHECK_THROW(combatTree.RemoveEdge(0, 0), std::invalid_argument);
+    BOOST_CHECK_THROW(combatTree.RemoveEdge(0, 1), std::invalid_argument);
+    BOOST_CHECK(combatTree.DoesVertexExist(0) == false);
+    BOOST_CHECK(combatTree.DoesEdgeExist(0, 1) == false);
+    BOOST_CHECK_THROW(combatTree.GetEdgeType(0, 1), std::invalid_argument);
+    BOOST_CHECK_THROW(combatTree.SetEdgeType(
+        0, 1, combat::EdgeType::Blocking), std::invalid_argument);
+
+    BOOST_CHECK(combatTree.CountAdjacent(0) == 0);
+    BOOST_CHECK(combatTree.AreAnyAdjacent(0) == false);
+    BOOST_CHECK(combatTree.GetBlockingPosMin() == 0);
+    BOOST_CHECK(combatTree.GetBlockingPosMax() == 0);
+    BOOST_CHECK(combatTree.GetBlockingDistanceMax() == 0);
+
+    {
+        combat::CombatTree::IdVec_t ids;
+        combatTree.GetNodeIds(ids, creature::role::Count);
+        BOOST_CHECK(ids.empty());
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        combatTree.GetNodeIds(ids, creature::race::Count);
+        BOOST_CHECK(ids.empty());
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        combatTree.FindAdjacentByEdgeType(0, ids);
+        BOOST_CHECK(ids.empty());
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        combatTree.FindAdjacent(0, ids);
+        BOOST_CHECK(ids.empty());
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        BOOST_CHECK(combatTree.GetNodeIDsAtBlockingPos(ids, 0) == false);
+        BOOST_CHECK(ids.empty());
+    }
+    {
+        combat::CombatTree::IdVec_t ids;
+        BOOST_CHECK(combatTree.GetNodeIDsAllAroundBlockingPos(ids, 0) == false);
+        BOOST_CHECK(ids.empty());
+    }
+    {
+        combat::CombatNodePVec_t combatNodePtrs;
+        BOOST_CHECK(combatTree.GetNodesAtBlockingPos(combatNodePtrs, 0) == false);
+        BOOST_CHECK(combatNodePtrs.empty());
+    }
+    {
+        combat::CombatNodePVec_t combatNodePtrs;
+        BOOST_CHECK(combatTree.GetNodesAllAroundBlockingPos(combatNodePtrs, 0) == false);
+        BOOST_CHECK(combatNodePtrs.empty());
+    }
+    {
+        combat::CombatNodePVec_t combatNodePtrs;
+        combatTree.GetCombatNodes(combatNodePtrs);
+        BOOST_CHECK(combatNodePtrs.empty());
+    }
 
     game::StartupShutdown::Teardown();
 }
