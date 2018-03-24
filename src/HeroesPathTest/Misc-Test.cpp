44fa32462ac69d0c859dfd7f91de1f3a1d7b2b25
Ziesche Til Newman <tilnewman@gmail.com>
2018-03-24 12:36:01 -0600

    Added a NotNull pointer wrapper.

--- src/HeroesPathTest/Misc-Test.cpp
+++ src/HeroesPathTest/Misc-Test.cpp
@@ -28,6 +28,7 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "misc/not-null.hpp"
 #include "misc/real.hpp"
 
 using namespace heroespath::misc;
@@ -68,4 +69,104 @@ BOOST_AUTO_TEST_CASE(Real_IsRealClose)
 
     BOOST_CHECK(IsRealClose(0.0, 1.0) == false);
     BOOST_CHECK(IsRealClose(0.0, 0.00001) == false);
+}
+
+BOOST_AUTO_TEST_CASE(NotNull_Tests)
+{
+    // heroespath::misc::NotNull<int *> notNull; //should not compile
+    // heroespath::misc::NotNull<int *> notNull{ nullptr }; //should not compile
+    // heroespath::misc::NotNull<int *> notNull{ 0 }; //should not compile
+
+    int * p0{ nullptr };
+    BOOST_CHECK_THROW(heroespath::misc::NotNull<int *> notNull0{ p0 }, std::exception);
+
+    int one{ 1 };
+    heroespath::misc::NotNull<int *> notNull1A{ &one };
+    BOOST_CHECK(notNull1A.Obj() == one);
+    BOOST_CHECK(notNull1A.Obj() == 1);
+    // BOOST_CHECK(notNull1A != nullptr); // should not compile
+    BOOST_CHECK(notNull1A.Ptr() == &one);
+    BOOST_CHECK(&one == notNull1A.Ptr());
+    BOOST_CHECK(notNull1A == &one);
+    BOOST_CHECK(&one == notNull1A);
+
+    BOOST_CHECK((notNull1A.Ptr() != &one) == false);
+    BOOST_CHECK((&one != notNull1A.Ptr()) == false);
+    BOOST_CHECK((notNull1A != &one) == false);
+    BOOST_CHECK((&one != notNull1A) == false);
+
+    int * p1{ &one };
+    heroespath::misc::NotNull<int *> notNull1B{ p1 };
+    BOOST_CHECK(notNull1B.Obj() == one);
+    BOOST_CHECK(notNull1B.Obj() == 1);
+
+    BOOST_CHECK(notNull1A == notNull1B);
+    BOOST_CHECK(notNull1A.Ptr() == notNull1B.Ptr());
+    BOOST_CHECK(notNull1A.Obj() == notNull1B.Obj());
+
+    heroespath::misc::NotNull<int *> notNull1C{ notNull1A };
+
+    BOOST_CHECK(notNull1A == notNull1C);
+    BOOST_CHECK(notNull1A.Ptr() == notNull1C.Ptr());
+    BOOST_CHECK(notNull1A.Obj() == notNull1C.Obj());
+
+    char c{ 'x' };
+    heroespath::misc::NotNull<char *> notNullC{ &c };
+
+    // BOOST_CHECK(notNull1A != notNullC); //should not compile
+    // BOOST_CHECK(notNull1A.Ptr() != notNullC.Ptr()); // should not compile
+
+    int * p2{ new int(2) };
+    heroespath::misc::NotNull<int *> notNull2{ p2 };
+
+    BOOST_CHECK(notNull2.Ptr() == p2);
+    BOOST_CHECK(p2 == notNull2.Ptr());
+    BOOST_CHECK(notNull2 == p2);
+    BOOST_CHECK(p2 == notNull2);
+
+    // heroespath::misc::NotNull<int> notNullX{ *p2 }; // should not compile
+
+    BOOST_CHECK(notNull1A != notNull2);
+    BOOST_CHECK(notNull1A.Ptr() != notNull2.Ptr());
+    BOOST_CHECK(notNull1A.Obj() != notNull2.Obj());
+
+    notNull1A = p2;
+
+    BOOST_CHECK(notNull1A == notNull2);
+    BOOST_CHECK(notNull1A.Ptr() == notNull2.Ptr());
+    BOOST_CHECK(notNull1A.Obj() == notNull2.Obj());
+
+    notNull1B = notNull2;
+
+    BOOST_CHECK(notNull1B == notNull2);
+    BOOST_CHECK(notNull1B.Ptr() == notNull2.Ptr());
+    BOOST_CHECK(notNull1B.Obj() == notNull2.Obj());
+
+    heroespath::misc::NotNull<int *> notNull3{ new int(3) };
+
+    BOOST_CHECK(notNull2 != notNull3);
+    BOOST_CHECK(notNull2.Ptr() != notNull3.Ptr());
+    BOOST_CHECK(notNull2.Obj() != notNull3.Obj());
+
+    *notNull3 = 2;
+
+    BOOST_CHECK(notNull2 != notNull3);
+    BOOST_CHECK(notNull2.Ptr() != notNull3.Ptr());
+    BOOST_CHECK(notNull2.Obj() == notNull3.Obj());
+
+    notNull3.Obj() = 1;
+    BOOST_CHECK(notNull3.Obj() == 1);
+
+    const heroespath::misc::NotNull<int *> NOTNULL4{ new int(4) };
+    BOOST_CHECK(NOTNULL4 != notNull3);
+    BOOST_CHECK(NOTNULL4.Ptr() != notNull3.Ptr());
+    BOOST_CHECK(NOTNULL4.Obj() != notNull3.Obj());
+
+    //*NOTNULL4 = 69; //should not compile
+    // NOTNULL4.Obj() = 69; // should not compile
+
+    // delete notNull2; should not compile
+    delete p2;
+    delete notNull3.Ptr();
+    delete NOTNULL4.Ptr();
 }
---
+++ src/misc/not-null.hpp
@@ -0,0 +1,159 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Heroes' Path - Open-source, non-commercial, simple, game in the RPG style.
+// Copyright (C) 2017 Ziesche Til Newman (tilnewman@gmail.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from
+// the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not
+//     claim that you wrote the original software.  If you use this software
+//     in a product, an acknowledgment in the product documentation would be
+//     appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not
+//     be misrepresented as being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
+//
+///////////////////////////////////////////////////////////////////////////////
+#ifndef HEROESPATH_MISC_NOT_NULL_HPP_INCLUDED
+#define HEROESPATH_MISC_NOT_NULL_HPP_INCLUDED
+//
+// not-null.hpp
+//
+#include "misc/assertlogandthrow.hpp"
+#include <algorithm>
+#include <iosfwd>
+#include <type_traits>
+#include <typeinfo>
+
+namespace heroespath
+{
+namespace misc
+{
+
+    // Responsible for wrapping a pointer that is guaranteed to never be null.
+    // Based on gsl::NotNull
+    template <typename T>
+    class NotNull
+    {
+    public:
+        static_assert(std::is_pointer<T>::value, "misc::NotNull<T> T must be pointer type.");
+
+        static_assert(
+            std::is_assignable<T &, std::nullptr_t>::value,
+            "misc::NotNull<T> cannot be assigned nullptr.");
+
+        template <typename U, typename = std::enable_if_t<std::is_convertible<U, T>::value>>
+        NotNull(U && u)
+            : ptr_(std::forward<U>(u))
+        {
+            M_ASSERT_OR_LOGANDTHROW_SS(
+                (ptr_ != nullptr),
+                "misc::NotNull<" << typeid(U).name() << ">::Constructor(&&) was given a nullptr.");
+        }
+
+        template <typename U, typename = std::enable_if_t<std::is_convertible<U, T>::value>>
+        NotNull(const NotNull<U> & other)
+            : NotNull(other.Ptr())
+        {}
+
+        NotNull(NotNull && other) = default;
+        NotNull(const NotNull & other) = default;
+        NotNull & operator=(const NotNull & other) = default;
+
+        T Ptr() const
+        {
+            M_ASSERT_OR_LOGANDTHROW_SS(
+                (ptr_ != nullptr),
+                "misc::NotNull<" << typeid(decltype(ptr_)).name()
+                                 << ">::Ptr() found ptr_==nullptr.");
+
+            return ptr_;
+        }
+
+        auto & Obj() { return *Ptr(); }
+        auto & operator-> () { return *Ptr(); }
+        auto & operator*() { return *Ptr(); }
+
+        const auto & Obj() const { return *Ptr(); }
+        const auto & operator-> () const { return *Ptr(); }
+        const auto & operator*() const { return *Ptr(); }
+
+        operator T() const = delete;
+
+        // prevents compilation when someone attempts to assign a null pointer constant
+        NotNull(std::nullptr_t) = delete;
+        NotNull & operator=(std::nullptr_t) = delete;
+
+        // unwanted operators...pointers only point to single objects!
+        NotNull & operator++() = delete;
+        NotNull & operator--() = delete;
+        NotNull operator++(int) = delete;
+        NotNull operator--(int) = delete;
+        NotNull & operator+=(std::ptrdiff_t) = delete;
+        NotNull & operator-=(std::ptrdiff_t) = delete;
+        void operator[](std::ptrdiff_t) const = delete;
+
+    private:
+        T ptr_;
+    };
+
+    template <typename T, typename U>
+    auto operator==(const NotNull<T> & L, const NotNull<U> & R) -> decltype(L.Ptr() == R.Ptr())
+    {
+        return (L.Ptr() == R.Ptr());
+    }
+
+    template <typename T, typename U>
+    auto operator!=(const NotNull<T> & L, const NotNull<U> & R) -> decltype(L.Ptr() != R.Ptr())
+    {
+        return (L.Ptr() != R.Ptr());
+    }
+
+    template <typename T>
+    bool operator==(const NotNull<T> & L, const T R)
+    {
+        return L.Ptr() == R;
+    }
+
+    template <typename T>
+    bool operator!=(const NotNull<T> & L, const T R)
+    {
+        return !(L == R);
+    }
+
+    template <typename T>
+    bool operator==(const T L, const NotNull<T> & R)
+    {
+        return L == R.Ptr();
+    }
+
+    template <typename T>
+    bool operator!=(const T L, const NotNull<T> & R)
+    {
+        return !(L == R);
+    }
+
+    // more unwanted operators
+    template <typename T, typename U>
+    std::ptrdiff_t operator-(const NotNull<T> &, const NotNull<U> &) = delete;
+
+    template <typename T>
+    NotNull<T> operator-(const NotNull<T> &, std::ptrdiff_t) = delete;
+
+    template <typename T>
+    NotNull<T> operator+(const NotNull<T> &, std::ptrdiff_t) = delete;
+
+    template <typename T>
+    NotNull<T> operator+(std::ptrdiff_t, const NotNull<T> &) = delete;
+}
+}
+
+#endif // HEROESPATH_MISC_NOT_NULL_HPP_INCLUDED
