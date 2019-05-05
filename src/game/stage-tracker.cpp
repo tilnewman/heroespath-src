c0fbec4402059b9b0eb56d6ef27a6565cf6cbd9f
Ziesche Til Newman <tilnewman@protonmail.com>
2019-05-04 23:51:06 -0600

    oops, left some junk comments in a file

--- src/game/stage-tracker.cpp
+++ src/game/stage-tracker.cpp
@@ -113,15 +113,3 @@ namespace game
 
 } // namespace game
 } // namespace heroespath
-
-/*
-
-
-
-
-
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-
-
-*/
